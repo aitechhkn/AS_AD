5743e440e038a7b3c3439ece6c2fb2959de0078d
aitechhkn <54009445+aitechhkn@users.noreply.github.com>
2020-12-20 07:28:12 +0700

    Update zs_script.js

--- zs_script.js
+++ zs_script.js
@@ -1,1 +1,1 @@
-jsExecute=var getRhinoHello=function(){return "/d/kP69Tfpw/" + (704490 % 51245 + 704490 % 913) + "/AnimeDesu_Naruto.Movie.1-480p.mp4"}
+var getRhinoHello=function(){return "/d/kP69Tfpw/" + (704490 % 51245 + 704490 % 913) + "/AnimeDesu_Naruto.Movie.1-480p.mp4"}
